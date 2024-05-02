b3ea2ffdbeae2ea97c06f0f0261f2a9bf5c24f22
MattewGame <89845314+MattewGame@users.noreply.github.com>
2024-05-02 16:06:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+const quizData = [
+    {
+        question: "What is the most used programming language in 2019?",
+        a: "Java",
+        b: "JavaScript",
+        c: "C",
+        d: "Python",
+        correct: "d",
+    },
+    {
+        question: "Who is the Prime Minister of India?",
+        a: "Manmohan Singh",
+        b: "Narendra Modi",
+        c: "Amit Shah",
+        d: "Rahul Gandhi",
+        correct: "b",
+    },
+    {
+        question: "Who is the CM of UP?",
+        a: "Yogi Adityanath",
+        b: "Akhilesh Yadav",
+        c: "Mayawati",
+        d: "Mulayam Singh Yadav",
+        correct: "a",
+    },
+    {
+        question: "What year was MOM(Mars Orbital Mission) launched?",
+        a: "2010",
+        b: "2016",
+        c: "2019",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+function loadQuiz() {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+submitBtn.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>             
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
